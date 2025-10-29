2a8377f4c2a3087cb8bc02f4322178dab4d3b263
xiaobin0630 <wangxiaobin_hfut@163.com>
2025-10-29 17:32:47 +0800

    增大抑制角度

--- launch/double_rplidar_s2_launch.py
+++ launch/double_rplidar_s2_launch.py
@@ -91,8 +91,8 @@ def generate_launch_description():
                          'angle_compensate': angle_compensate,
                          'scan_mode': scan_mode,
                          'topic_name': front_scan_topic,
-                         'negative_angle_cut_val': -0.85,
-                         'positive_angle_cut_val': 0.85
+                         'negative_angle_cut_val': -0.9,
+                         'positive_angle_cut_val': 0.9
                          }],
             respawn=True,
             respawn_delay=5.0,
@@ -110,7 +110,7 @@ def generate_launch_description():
                          'angle_compensate': angle_compensate,
                          'scan_mode': scan_mode,
                          'topic_name': back_scan_topic,
-                         'negative_angle_cut_val': -0.85,
+                         'negative_angle_cut_val': -0.9,
                          'positive_angle_cut_val': 0.9
                          }],
             respawn=True,
